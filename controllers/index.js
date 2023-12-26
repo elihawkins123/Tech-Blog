f7fd1336ea64f3500fa170d1ed70cef2812eac85
elihawkins123 <elizabethrhawkins123@gmail.com>
2023-12-25 20:51:23 -0700

    add dashboard routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const dashboardRoutes = require('./dashboardRoutes');
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
